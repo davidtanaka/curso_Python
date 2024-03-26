09df835b615619f92dcd0c8d88f249bd2f3dee05
davi dias tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-03-26 17:20:11 -0300

    Atualizando aula 115 e gitignore

--- aula115/aula115.py
+++ aula115/aula115.py
@@ -26,8 +26,8 @@
 # Instalar versão precisa
 # (tem outras formas também não mencionadas)
 # pip install nome_pacote==0.0.0
-# Desinstalar pacote
-# pip uninstall nome_pacote
+# Desinstalar pacote 
+# pip uninstall nome_pacote 
 # Congelar (ver pacotes)
 # pip freeze
 
