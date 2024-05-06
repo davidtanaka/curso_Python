d977d16606089a39ee116d47ffe0feb60a0158c3
davi dias tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-05-05 21:16:09 -0300

    Aula 160
    namedtuple

---
+++ aula160/aula160.py
@@ -0,0 +1,33 @@
+# namedtuples - tuplas imutáveis com nomes para valores
+# Usamos namedtuples para criar classes de objetos que são apenas um
+# agrupamento de atributos, como classes normais sem métodos, ou registros de
+# bases de dados, etc.
+# As namedtuples são imutáveis assim como as tuplas.
+# https://docs.python.org/3/library/collections.html#collections.namedtuple
+# https://docs.python.org/3/library/typing.html#typing.NamedTuple
+# https://brasilescola.uol.com.br/curiosidades/baralho.htm
+# from collections import namedtuple
+from typing import NamedTuple
+
+class Carta(NamedTuple):
+    valor: str = 'VALOR'
+    naipe: str = 'NAIPE'
+
+
+# Carta = namedtuple(
+#     'Carta', ['valor', 'naipe'],
+#     defaults=['VALOR', 'NAIPE']
+# )
+
+
+
+as_espada = Carta('A', '♠️')
+# print(as_espada)
+# print(as_espada[0])
+# print(as_espada.valor)
+# print(as_espada[1])
+# print(as_espada.naipe)
+print(as_espada._field_defaults)
+
+for valor in as_espada:
+    print(valor)
