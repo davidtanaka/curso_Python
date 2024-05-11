40b68bad3f70058c1fff97416353b700f031e53b
davi dias tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-05-11 15:41:47 -0300

    Aula 173
    Aprendendo a criar novas pastas e copiar arquivos para pastas com os

---
+++ aula173/aula173.py
@@ -0,0 +1,27 @@
+# os + shutil - Copiando arquivos com Python
+# Vamos copiar arquivos de uma pasta para outra.
+# Copiar -> shutil.copy
+import os
+import shutil
+
+HOME = os.path.expanduser('~')
+DUCUMENTS = os.path.join(HOME, 'Documents')
+PASTA_ORIGINAL = os.path.join(DUCUMENTS, 'EXEMPLO')
+NOVA_PASTA = os.path.join(DUCUMENTS, 'NOVA_PASTA')
+
+os.makedirs(NOVA_PASTA, exist_ok=True)
+
+for root, dirs, files in os.walk(PASTA_ORIGINAL):
+    for dir_ in dirs:
+        novo_caminho_diretorio = os.path.join(
+        root.replace(PASTA_ORIGINAL, NOVA_PASTA), dir_
+        )
+        os.makedirs(novo_caminho_diretorio, exist_ok=True)
+
+
+    for file in files:
+        caminho_arquivo = os.path.join(root, file)
+        novo_caminho_arquivo = os.path.join(
+            root.replace(PASTA_ORIGINAL, NOVA_PASTA), file
+        )
+        shutil.copy(caminho_arquivo, novo_caminho_arquivo)
