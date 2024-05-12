89ab1ae2b7d0ea48143b6efa98717709d15bd406
davi dias tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-05-12 16:11:38 -0300

    Aula 174

---
+++ aula174/aula174.py
@@ -0,0 +1,38 @@
+# os + shutil - Apagando, copiando, movendo e renomeando pastas com Python
+# Vamos copiar arquivos de uma pasta para outra.
+# Copiar -> shutil.copy
+# Copiar Árvore recursivamente -> shutil.copytree
+# Apagar Árvore recursivamente -> shutil.rmtree
+# Apagar arquivos -> os.unlink
+# Renomear/Mover -> shutil.move ou os.rename
+from genericpath import exists
+from logging import raiseExceptions
+import os
+import shutil
+
+HOME = os.path.expanduser('~')
+DUCUMENTS = os.path.join(HOME, 'Documents')
+PASTA_ORIGINAL = os.path.join(DUCUMENTS, 'EXEMPLO')
+NOVA_PASTA = os.path.join(DUCUMENTS, 'NOVA_PASTA')
+
+shutil.rmtree(NOVA_PASTA, ignore_errors=True)
+shutil.copytree(PASTA_ORIGINAL, NOVA_PASTA)
+shutil.move(NOVA_PASTA, NOVA_PASTA + '_EITA')
+
+"""os.makedirs(NOVA_PASTA, exist_ok=True)
+
+for root, dirs, files in os.walk(PASTA_ORIGINAL):
+    for dir_ in dirs:
+        novo_caminho_diretorio = os.path.join(
+        root.replace(PASTA_ORIGINAL, NOVA_PASTA), dir_
+        )
+        os.makedirs(novo_caminho_diretorio, exist_ok=True)
+
+
+    for file in files:
+        caminho_arquivo = os.path.join(root, file)
+        novo_caminho_arquivo = os.path.join(
+            root.replace(PASTA_ORIGINAL, NOVA_PASTA), file
+        )
+        shutil.copy(caminho_arquivo, novo_caminho_arquivo)
+"""
