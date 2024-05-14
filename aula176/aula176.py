a5eecdbb8c9f6ef38e1a25b6960bb411212f27d4
davi dias tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-05-14 20:10:48 -0300

    Aula 175 e 176

---
+++ aula176/aula176.py
@@ -0,0 +1,42 @@
+# json.dumps e json.loads com strings + typing.TypedDict
+# Ao converter de Python para JSON:
+# Python        JSON
+# dict          object
+# list, tuple   array
+# str           string
+# int, float    number
+# True          true
+# False         false
+# None          null
+import json
+from pprint import pprint
+from typing import TypedDict
+
+class Movie(TypedDict):
+  title: str
+  original_title: str
+  is_movie: bool 
+  imdb_rating: float
+  year: int
+  characters: list[str]
+  budget: None | float
+
+string_json = '''
+{
+  "title": "O Senhor dos Anéis: A Sociedade do Anel",
+  "original_title": "The Lord of the Rings: The Fellowship of the Ring",
+  "is_movie": true,
+  "imdb_rating": 8.8,
+  "year": 2001,
+  "characters": ["Frodo", "Sam", "Gandalf", "Legolas", "Boromir"],
+  "budget": null
+}
+'''
+movie: Movie = json.loads(string_json)
+# pprint(movie)
+# print(movie['title'])
+# print(movie['characters'][0])
+# print(movie['year'] + 10)
+
+json_string = json.dumps(movie, ensure_ascii=False, indent=2)
+print(json_string)
