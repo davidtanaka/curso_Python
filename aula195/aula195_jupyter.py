6e47a2cf83b52173a5ecdcaecfd8f78a966c87b2
davi dias tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-05-25 22:49:22 -0300

    Aprendendo sobre jupyter

---
+++ aula195/aula195_jupyter.py
@@ -0,0 +1,10 @@
+# Jupyter Nootebok - Instalação e teste 
+
+# Essa linha cria uma variavel a 
+a =2
+
+# Essa linha tem o valor da variável `a` em dobro 
+b = a * 2
+
+# Essa linha mostra o valor de `a` + `b`
+print(a + b) # 6
