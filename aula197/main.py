95f36a9177c83730614f0e78d63ce9f24e0a718a
davi dias tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-05-27 14:36:36 -0300

    PyPDF2 para manipular arquivos PDF (PdfReader)
    Extraindo imagem do pdf

--- aula197/main.py
+++ aula197/main.py
@@ -7,3 +7,25 @@
 # Link: https://pypdf2.readthedocs.io/en/3.0.0/
 # Ative seu ambiente virtual
 # pip install pypdf2
+from genericpath import exists
+from pathlib import Path 
+from PyPDF2 import PdfReader
+
+PASTA_RAIZ = Path(__file__).parent
+PASTA_ORIGINAIS = PASTA_RAIZ / 'pdfs_originais'
+PASTA_NOVA = PASTA_RAIZ / 'arquivos_novos'
+
+RELATORIO_BACEN = PASTA_ORIGINAIS / 'R20230210.pdf'
+PASTA_NOVA.mkdir(exist_ok=True)
+
+reader = PdfReader(RELATORIO_BACEN)
+# print(len(reader.pages))
+# for page in reader.pages:
+#     print(page)
+
+page0 = reader.pages[0]
+imagem0 = page0.images[0]
+
+# print(page0.extract_text())
+with open(PASTA_NOVA/page0.images[0].name, 'wb') as fp:
+    fp.write(imagem0.data)
