a1653c09c4f1058c1e19fa9970cbbebe1e58a9f3
davi dias tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-06-07 19:02:23 -0300

    Calculadora: criando um QLabel para mostrar informações

--- aula207-calculadora/main.py
+++ aula207-calculadora/main.py
@@ -1,5 +1,6 @@
 import sys
 from main_window import MainWindow
+from info import Info
 from display import Display
 from PySide6.QtWidgets import QApplication
 from variables import WINDOW_ICON_PATH
@@ -14,6 +15,10 @@ if __name__ == '__main__':
     icon = QIcon(str(WINDOW_ICON_PATH))
     window.setWindowIcon(icon)
     app.setWindowIcon(icon)
+
+    # Info
+    info = Info('2.0 ^ 10.0 = 1024')
+    window.addToVLayout(info)
 
     # Display
     display = Display()
