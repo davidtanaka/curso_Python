9f41ef0099ca0ba02d00ba1693d5d9e7350c2e39
davi dias tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-06-06 21:15:11 -0300

    Calculadora: QLineEdit e o display

--- aula207-calculadora/main.py
+++ aula207-calculadora/main.py
@@ -1,6 +1,7 @@
 import sys
 from main_window import MainWindow
-from PySide6.QtWidgets import QApplication, QLabel
+from display import Display
+from PySide6.QtWidgets import QApplication
 from variables import WINDOW_ICON_PATH
 from PySide6.QtGui import QIcon
 
@@ -13,6 +14,10 @@ if __name__ == '__main__':
     icon = QIcon(str(WINDOW_ICON_PATH))
     window.setWindowIcon(icon)
     app.setWindowIcon(icon)
+
+    # Display
+    display = Display()
+    window.addToVLayout(display)
 
     # Condição para aparecer o ícone na barra de tarefas (windows).
     if sys.platform.startswith('win'):
--- aula207-calculadora/variables.py
+++ aula207-calculadora/variables.py
@@ -3,3 +3,10 @@ from pathlib import Path
 Root_DIR = Path(__file__).parent
 FILES_DIR = Root_DIR / 'files'
 WINDOW_ICON_PATH = FILES_DIR / 'icon.png'
+
+# Sizing
+BIG_FONT_SIZE = 40
+MEDIUM_FONT_SIZE = 24
+SMALL_FONT_SIZE = 18
+TEXT_MARGIN = 15
+MINIMUN_WIDTH = 500
