4db25a1b5947e9d5d183a5ba8fa388bde2d1f2e6
davi dias tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-06-08 23:14:17 -0300

    Calculadora: criando um Slot com dados para o Signal "clicked" de cad…

--- aula207-calculadora/buttons.py
+++ aula207-calculadora/buttons.py
@@ -1,7 +1,8 @@
-from PySide6.QtWidgets import QPushButton, QGridLayout
+from display import Display
+from PySide6.QtCore import Slot
+from PySide6.QtWidgets import QGridLayout, QPushButton
+from utils import isEmpty, isNumOrDot
 from variables import MEDIUM_FONT_SIZE
-from styles import qss
-from utils import isNumOrDot, isEmpty
 
 class Button(QPushButton):
     def __init__(self, *args, **kwargs):
@@ -13,10 +14,10 @@ class Button(QPushButton):
         font.setPixelSize(MEDIUM_FONT_SIZE)
         self.setFont(font)
         self.setMinimumSize(75, 75)
-
+        self.setCheckable(True)
 
 class ButtonsGrid(QGridLayout):
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, display: Display, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
         self._gridMask = [
@@ -26,6 +27,7 @@ class ButtonsGrid(QGridLayout):
             ['1', '2', '3', '+'],
             ['',  '0', '.', '='],
         ]
+        self.display = display
         self._makeGrid()
 
 
@@ -37,4 +39,19 @@ class ButtonsGrid(QGridLayout):
                 if not isNumOrDot(buttonText) and not isEmpty:
                             button.setProperty('cssClass', 'specialButton')
                             button.setStyleSheet(qss) # type: ignore
-                self.addWidget(button, i, j)
+                self.addWidget(button, i, j)  
+                buttonSlot = self._makeButtonDisplaySlot(
+                    self._insertButtonTextToDisplay,
+                    button,
+                )
+                button.clicked.connect(buttonSlot)  # type: ignore
+
+    def _makeButtonDisplaySlot(self, func, *args, **kwargs):
+        @Slot(bool)
+        def realSlot(_):
+            func(*args, **kwargs)
+        return realSlot
+
+    def _insertButtonTextToDisplay(self, button):
+        button_text = button.text()
+        self.display.insert(button_text)
--- aula207-calculadora/styles.py
+++ aula207-calculadora/styles.py
@@ -7,15 +7,19 @@ import qdarktheme
 from variables import PRIMARY_COLOR, DARKER_PRIMARY_COLOR, DARKEST_PRIMARY_COLOR
 
 qss = f"""
-    PushButton[cssClass="specialButton"] {{
+    QPushButton[cssClass="specialButton"] {{
         color: #fff;
         background: {PRIMARY_COLOR};
     }}
-    PushButton[cssClass="specialButton"]:hover {{
+    QPushButton[cssClass="specialButton"]:hover {{
         color: #fff;
         background: {DARKER_PRIMARY_COLOR};
     }}
-    PushButton[cssClass="specialButton"]:pressed {{
+    QPushButton[cssClass="specialButton"]:pressed {{
+        color: #fff;
+        background: {DARKEST_PRIMARY_COLOR};
+    }}
+    QPushButton[cssClass="specialButton"]:pressed {{
         color: #fff;
         background: {DARKEST_PRIMARY_COLOR};
     }}
