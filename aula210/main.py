162981e70a462218279dbb0267113dd1e603ab32
Bowie <134816317+davidtanaka@users.noreply.github.com>
2024-06-27 14:24:08 -0300

    Criando tabela no SQLite3

--- aula210/main.py
+++ aula210/main.py
@@ -4,11 +4,21 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parent
 DB_NAME = 'db.sqlite2'
 DB_FILE = ROOT_DIR / DB_NAME
+TABLE_NAME = 'customers'
 
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
 # SQL
+cursor.execute(
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+    '('
+    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+    'name TEXT,'
+    'weight TEXT'
+    ')'
+)
+connection.commit()
 
 cursor.close()
 connection.close()
