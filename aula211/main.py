04904555f79e4543a98505ef73f1a5b0530ceb0c
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-07-05 23:06:32 -0300

    Lendo valores com select PyMySQL

--- aula211/main.py
+++ aula211/main.py
@@ -2,26 +2,26 @@
 # Doc: https://pymysql.readthedocs.io/en/latest/
 # Pypy: https://pypi.org/project/pymysql/
 # GitHub: https://github.com/PyMySQL/PyMySQL
-import pymysql
-import dotenv
 import os
 
-TABLE_NAME = 'customers'
-dotenv.load_dotenv()
+import dotenv
+import pymysql
 
+TABLE_NAME = 'customers'
+
+dotenv.load_dotenv()
 
 connection = pymysql.connect(
     host=os.environ['MYSQL_HOST'],
     user=os.environ['MYSQL_USER'],
     password=os.environ['MYSQL_PASSWORD'],
     database=os.environ['MYSQL_DATABASE'],
+    charset='utf8mb4'
 )
-
-# print(os.environ['MYSQL_DATABASE'])
 
 with connection:
     with connection.cursor() as cursor:
-        cursor.execute(
+        cursor.execute(  # type: ignore
             f'CREATE TABLE IF NOT EXISTS {TABLE_NAME} ('
             'id INT NOT NULL AUTO_INCREMENT, '
             'nome VARCHAR(50) NOT NULL, '
@@ -29,81 +29,88 @@ with connection:
             'PRIMARY KEY (id)'
             ') '
         )
-        # ISSO LIMPA A TABELA
-        cursor.execute(f'TRUNCATE TABLE {TABLE_NAME}')
-        connection.commit()
-
-        with connection.cursor() as cursor:
-            sql =(f'INSERT INTO {TABLE_NAME} (nome, idade) VALUES (%s, %s)')
-            data = ("Davi", 16)
-            result = cursor.execute(sql, data)
-            # print(sql)
-            # print(data)
-            # print(result)
-        connection.commit()
-
-
-        with connection.cursor() as cursor:
-            sql2 =(f'INSERT INTO {TABLE_NAME} (nome, idade)'
-            ' VALUES (%(name)s, %(age)s) '
-        )
-            data2 = {
-                "age": 16,
-                "name": "De",
-            }
-            
-            result2 = cursor.execute(sql2, data2)
-            # print(sql2)
-            # print(data2)
-            # print(result2)
-        connection.commit()
-
-
-
-        with connection.cursor() as cursor:
-            sql3 =(f'INSERT INTO {TABLE_NAME} (nome, idade)'
-            ' VALUES (%(name)s, %(age)s) '
-        )
-            data3 = (
-                {"age": 18, "name": "Mauro", },
-                {"age": 19, "name": "Julia", },
-                {"age": 83, "name": "Ione", },
-            )
-            
-            result2 = cursor.executemany(sql3, data3)
-            # print(sql3)
-            # print(data3)
-            # print(result2)
-        connection.commit()
-
-        
-    with connection.cursor() as cursor:
-            sql2 =(f'INSERT INTO {TABLE_NAME} (nome, idade)'
-            ' VALUES (%(name)s, %(age)s) '
-        )
-            data2 = {
-                "age": 16,
-                "name": "De",
-            }
-            
-            result3 = cursor.execute(sql2, data2)
-            # print(sql2)
-            # print(data2)
-            # print(result3)
+        # CUIDADO: ISSO LIMPA A TABELA
+        cursor.execute(f'TRUNCATE TABLE {TABLE_NAME}')  # type: ignore
     connection.commit()
 
+    # Começo a manipular dados a partir daqui
 
+    # Inserindo um valor usando placeholder e um iterável
     with connection.cursor() as cursor:
-        sql4 =(f'INSERT INTO {TABLE_NAME} (nome, idade)'
-               ' VALUES (%s, %s) '
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) '
+            'VALUES '
+            '(%s, %s) '
+        )
+        data = ('Luiz', 18)
+        result = cursor.execute(sql, data)  # type: ignore
+        # print(sql, data)
+        # print(result)
+    connection.commit()
+
+    # Inserindo um valor usando placeholder e um dicionário
+    with connection.cursor() as cursor:
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) '
+            'VALUES '
+            '(%(name)s, %(age)s) '
+        )
+        data2 = {
+            "age": 37,
+            "name": "Le",
+        }
+        result = cursor.execute(sql, data2)  # type: ignore
+        # print(sql)
+        # print(data2)
+        # print(result)
+    connection.commit()
+
+    # Inserindo vários valores usando placeholder e um tupla de dicionários
+    with connection.cursor() as cursor:
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) '
+            'VALUES '
+            '(%(name)s, %(age)s) '
+        )
+        data3 = (
+            {"name": "Sah", "age": 33, },
+            {"name": "Júlia", "age": 74, },
+            {"name": "Rose", "age": 53, },
+        )
+        result = cursor.executemany(sql, data3)  # type: ignore
+        # print(sql)
+        # print(data3)
+        # print(result)
+    connection.commit()
+
+    # Inserindo vários valores usando placeholder e um tupla de tuplas
+    with connection.cursor() as cursor:
+        sql = (
+            f'INSERT INTO {TABLE_NAME} '
+            '(nome, idade) '
+            'VALUES '
+            '(%s, %s) '
         )
         data4 = (
-            ("Clau", 43),
-            ("Jandrei", 29),
-            )
-            
-        result4 = cursor.executemany(sql4, data4)
-        print(sql4)
-        print(data4)
-        print(result4)
-        connection.commit()
+            ("Siri", 22, ),
+            ("Helena", 15, ),
+        )
+        result = cursor.executemany(sql, data4)  # type: ignore
+        # print(sql)
+        # print(data4)
+        # print(result)
+    connection.commit()
+
+    # Lendo os valores com SELECT
+    with connection.cursor() as cursor:
+        sql = (
+            f'SELECT * FROM {TABLE_NAME} '
+        )
+        cursor.execute(sql)  # type: ignore
+        data5 = cursor.fetchall()  # type: ignore
+
+        for row in data5:
+            print(row)
