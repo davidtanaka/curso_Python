7962cf8d752be331d1450ad860539233f1a78bf2
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-10-15 21:48:04 -0300

    Aprendendo sobre: Asserções (Assertions)

--- python-tests/calculadora.py
+++ python-tests/calculadora.py
@@ -1,3 +1,5 @@
-def soma(x, y):
+def soma(x: int, y: int):
+    assert isinstance(x, (int, float)), 'x precisa ser int ou float'
+    assert isinstance(y, (int, float)), 'y precisa ser int ou float'
     return x + y
 
--- python-tests/main.py
+++ python-tests/main.py
@@ -1,3 +1,10 @@
 from calculadora import soma
 
 print(soma(2, 4))
+print(soma(12, -10))
+print(soma(11, 9))
+
+try:
+    print(soma('12', -1)) # type: ignore
+except AssertionError as e:
+    print(f'Conta ínvalida: {e}')
