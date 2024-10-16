f064adc1107bd40ca26e84464bd4cf93a9b9af8b
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-10-15 23:01:57 -0300

    Apredendo sobre Doctests

--- python-tests/calculadora.py
+++ python-tests/calculadora.py
@@ -1,5 +1,29 @@
-def soma(x: int, y: int):
+def soma(x, y):
+    """ Soma x e y
+    >>> soma(10, 20)
+    30
+
+    >>> soma(-10, 20)
+    10
+    """
     assert isinstance(x, (int, float)), 'x precisa ser int ou float'
     assert isinstance(y, (int, float)), 'y precisa ser int ou float'
     return x + y
 
+def subtrai(x, y):
+    """ Subtrai x e y
+
+    >>> subtrai(10, 5)
+    5
+
+    >>> subtrai(10, 4)
+    6
+    
+    """
+    assert isinstance(x, (int, float)), 'x precisa ser int ou float'
+    assert isinstance(y, (int, float)), 'y precisa ser int ou float'
+    return x - y
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
