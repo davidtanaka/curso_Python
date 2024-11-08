c0271bc75c918b5cc0375ada15d35360e8a246ad
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-11-08 17:56:33 -0300

    Update state_1.py

--- design-patterns/behavioral/state/src/state_1.py
+++ design-patterns/behavioral/state/src/state_1.py
@@ -1,3 +1,11 @@
+"""
+O Padrão de projeto State é um padrão comportamental
+que tem a intenção de permitir a um objeto mudar
+seu comportamento quando o seu estado interno
+muda.
+O objeto parecerá ter mudado sua classe.
+"""
+
 from abc import ABC, abstractmethod
 
 class Order:
