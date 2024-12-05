fb01078e2037f6c645523398be64b7178a30ed86
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-12-05 14:40:28 -0300

    Aula 14 -  Validando números

---
+++ regexp-python/aula14/aula.py
@@ -0,0 +1,61 @@
+# https://regex101.com/r/wjfSus/1/
+import re
+
+string = '''
+Válidos
+0.0
+00.00
+000.000
++0.0
+-00.00
++000.000
+10
+50
+8.5
+-8.5
++10.5005412136
+1231345458.54654564
+-133215646589.6543215648978977
++1.11123123
+-0.154987
+1.121654987
+10.123
+10.1
+-1.1
+
+Inválidos
+10..2
+++1213
+--1235050
+.124546
+-.1231324
+10.
+.1
+.10
+10.
++10.
+-10.
+5a
+b5
+'''
+# valid_numbers = re.compile(r'^[-+]?[0-9]+\.[0-9]+$', flags=re.M)
+# print(valid_numbers.findall(string))
+
+def is_float(string) -> bool:
+    return bool(re.search(r'^[+-]?\d+(?:\.\d+)?$', string))
+
+def is_int(string) -> bool:
+    return bool(re.search(r'^[+-]?\d+$', string))
+
+while True:
+    numero = input('Digite um número: ')
+
+    if is_int(numero):
+        numero = int(numero)
+        print(f'O número {numero} foi convertido para INT')
+        continue
+
+    if is_float(numero):
+        numero = float(numero)
+        print(f'O número {numero} foi convertido para FLOAT')
+        continue
